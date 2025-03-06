d457f9f8e1b5d6cee925144539f580b588e84d2a
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2025-03-06 19:20:50 +0100

    chores: add one more test

--- test/clj/big_config/main_test.clj
+++ test/clj/big_config/main_test.clj
@@ -3,7 +3,7 @@
    [big-config.main :refer [run-with-lock]]
    [clojure.test :refer [deftest is testing]]))
 
-(deftest run-with-lock-test
+(deftest run-with-lock-with-true-test
   (testing "true"
     (let [opts {:aws-account-id "111111111111"
                 :region "eu-west-1"
@@ -16,3 +16,20 @@
           end-fn (partial swap! xs conj)]
       (run-with-lock opts end-fn)
       (is (every? #(= (:exit %) 0) @xs)))))
+
+(deftest run-with-lock-with-false-test
+  (testing "first false then true"
+    (let [opts   {:aws-account-id "111111111111"
+                  :region         "eu-west-1"
+                  :ns             "test.module"
+                  :fn             "invoke"
+                  :owner          "CI"
+                  :lock-keys      [:aws-account-id :region :ns]
+                  :run-cmd        "false"}
+          xs     (atom [])
+          end-fn (partial swap! xs conj)]
+      (run-with-lock opts end-fn)
+      (run-with-lock (assoc opts :run-cmd "true") end-fn)
+      (as-> @xs $
+        (map :exit $)
+        (is (= [1 0] $))))))
