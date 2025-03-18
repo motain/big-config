b6acb623f843580168d80b7fa60376c90070df05
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2025-03-18 08:48:52 +0100

    refactor: better msgs

--- src/clj/big_config/msg.clj
+++ src/clj/big_config/msg.clj
@@ -5,14 +5,14 @@
 (defn step->message
   "no require to avoid circular dependencies"
   [step]
-  (let [messages {:big-config.aero/read-module "Reading the module"
-                  :big-config.run/generate-main-tf-json "Generating the main.tf.json file"
-                  :big-config.run/run-cmd "Running the tofu command"
+  (let [messages {:big-config.aero/read-module "Reading aero module"
+                  :big-config.run/generate-main-tf-json "Generating main.tf.json file"
+                  :big-config.run/run-cmd "Running tofu command"
                   :big-config.run-with-lock/lock-acquire "Acquiring lock"
                   :big-config.run-with-lock/git-check "Checking if there are files not in the index"
-                  :big-config.run-with-lock/generate-main-tf-json "Generating the main.tf.json file"
-                  :big-config.run-with-lock/run-cmd "Running the tofu command"
-                  :big-config.run-with-lock/git-push "Pushing the changes to git"
+                  :big-config.run-with-lock/generate-main-tf-json "Generating main.tf.json file"
+                  :big-config.run-with-lock/run-cmd "Running tofu command"
+                  :big-config.run-with-lock/git-push "Pushing changes to git"
                   :big-config.run-with-lock/lock-release-any-owner "Releasing lock"}
         msg (step messages)]
     (when msg
